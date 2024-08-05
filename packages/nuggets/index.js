1527ccc6d4a4554667a1b271b7fe3486f668e21b
Brian Winkers <bwinkers@gmail.com>
2024-08-05 14:58:01 -0700

    Fresh start 3

--- packages/nuggets/index.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    console.log("Loaded package @ultri/nuggets");
-};
--- packages/spaces/index.js
+++
@@ -1,6 +0,0 @@
-import multicorder from "@ultri/multicorder";
-multicorder();
-
-export default () => {
-    console.log("Loaded package @ultri/spaces");
-};
